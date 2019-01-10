100b24c705eafa4c8823cc80222c83b1aad108ce
simon.carron <simon.carron@gmail.com>
2019-01-10 17:11:07 +0100

    all

---
+++ Orchestration/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import render_template, Flask, session
+
+from module.auth import auth_blueprint
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+        return render_template('index.html', title="Home", user=session)
+
+app.register_blueprint(auth_blueprint)
